2713fae73fdccd59e3d52e986664fd18d8794c96
cable <cable870526@gmail.com>
2023-10-02 11:53:49 +0800

    if+switch

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$dividend = 101; // 被除数
+$divisor = 2;  // 除数
+
+$remainder = $dividend % $divisor; // 使用取模运算符计算余数
+
+if ($remainder==1) {
+     echo $dividend."為奇數";
+}
+else {
+    echo $dividend."為偶數";
+}
+?>
