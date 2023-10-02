5252f4732046fbd908af3ad443688c018085ff48
cable <cable870526@gmail.com>
2023-10-02 10:40:30 +0800

    if

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$a=10;
+$b=50;
+echo$a;
+echo$b;
+$c=$a;
+$a=$b;
+$b=$c;
+echo$a;
+echo$b;
+
+?>
+</body>
+</html>
