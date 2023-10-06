a6ce4828084c5c4fd115249b5416339fdc7ca50f
cable <cable870526@gmail.com>
2023-10-06 14:26:35 +0800

    foreach用法

--- test2.php
+++ test2.php
@@ -20,28 +20,45 @@
 //     else{
 //         echo $a.'不是質數'; 
 //     }
-    $n=100;
-    $count=0;
-    for($j=2;$j<=$n;$j++){
-        $flag=true;
-        
-        for($i=2;$i<=($j/2);$i++){
-            $count++;
-            //echo "$j 除以 $i 餘數 ". ($j % $i);
-            if(($j % $i) == 0){
-                $flag=false;
-              //  echo "<br>";
-                break;
-            }
-            //echo "<br>";
+$n = 100;
+$count = 0;
+for ($j = 2; $j <= $n; $j++) {
+    $flag = true;
+
+    for ($i = 2; $i <= ($j / 2); $i++) {
+        $count++;
+        //echo "$j 除以 $i 餘數 ". ($j % $i);
+        if (($j % $i) == 0) {
+            $flag = false;
+            //  echo "<br>";
+            break;
         }
-        
-        if($flag==true){
-            echo $j ;
-            echo ",";
-        }else{
-            //echo $j . "不是質數";
-        }
+        //echo "<br>";
     }
+
+    if ($flag == true) {
+        echo $j;
+        echo ",";
+    } else {
+        //echo $j . "不是質數";
+    }
+}
+echo "<br>";
+echo "一共執行了" . $count . "次的程式內容";
+echo "<br>";
+
+
+$a = ["甲", "乙", "丙"];
+for ($i = 0; $i < count($a); $i++) {
+    echo $a[$i];
+}
+echo "<br>";
+
+foreach ($a as $b) {
+    echo $b;
+}
+echo "<br>";
+foreach ($a as $idx => $b) {
+    echo $idx . "=>" .$b;
     echo "<br>";
-    echo "一共執行了".$count."次的程式內容";
+}
