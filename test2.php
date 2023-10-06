dac76ece76854060395a3ca580564137e85cff47
cable <cable870526@gmail.com>
2023-10-06 10:56:12 +0800

    找質數for迴圈

--- test2.php
+++ test2.php
@@ -1,5 +1,21 @@
 <?php
-for ($i=3; $i<=97 ; $i=$i+2) { 
-    echo $i;
-    echo "<br>";
+// $n=30;
+// for ($i=1; $i<=$n ; $i=$i+1) { 
+//     echo $i;
+//     echo',';
+// }
+$a=97;
+$flag=true;
+for ($i=2; $i<($a/2) ; $i++) { 
+    $a%$i;
+    if (($a%$i)==0) {
+        $flag=false;
+        break;
+    }
 }
+    if ( $flag=true) {
+       echo $a.'是質數';
+    }
+    else{
+        echo $a.'不是質數'; 
+    }
