21d1f3ce2381dd3d986ad546a05fb5c1ab014a8d
cable <cable870526@gmail.com>
2023-10-13 15:46:48 +0800

    for、if迴圈做星星矩形+css

--- 20231013/pra01.php
+++ 20231013/pra01.php
@@ -142,9 +142,13 @@ for ($i = 0; $i < 7; $i++) {
     for ($j = 0; $j < 7; $j++) {
         if ($i == 0 || $i == 6) {
             echo "*";
-        } else if ($j == 0 || $j == 6 ||$i==$j || $i+$j==6) {
+        } else if ($j == 0 || $j == 6 ) {
             echo "*";
-        }else {
+        }else if($i==$j || $i+$j==6){
+            // span行內標籤
+            echo "<span style='color:red'>*</span>";
+        }
+        else{
             echo "&nbsp";
         }
     }
