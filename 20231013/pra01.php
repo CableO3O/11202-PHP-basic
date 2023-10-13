f04e11f22bb87482478371c5f5456b4831d80bf6
cable <cable870526@gmail.com>
2023-10-13 16:16:08 +0800

    for、if將所有形狀用一個變數更改

--- 20231013/pra01.php
+++ 20231013/pra01.php
@@ -32,7 +32,10 @@
 // 3   3
 // 4   4
 // 5   5
-for ($i = 0; $i <= 5; $i++) {
+$amount=3;
+
+
+for ($i = 0; $i <= $amount; $i++) {
     for ($j = 0; $j < $i; $j++) {
         echo "*";
     }
@@ -43,7 +46,7 @@ for ($i = 0; $i <= 5; $i++) {
 ?>
 <h1>星星倒直角三角形</h1>
 <?php
-for ($i = 4; $i >= 0; $i--) {
+for ($i = $amount; $i >= 0; $i--) {
     for ($j = 0; $j <= $i; $j++) {
         echo "*";
     }
@@ -61,9 +64,9 @@ for ($i = 4; $i >= 0; $i--) {
 <h1>星星正三角形</h1>
 <?php
 // 第1排決定共幾排星星
-for ($i = 0; $i < 5; $i++) {
+for ($i = 0; $i < $amount; $i++) {
     //  第2排決定每排打幾個空排
-    for ($j = 0; $j < (4 - $i); $j++) {
+    for ($j = 0; $j < (($amount-1) - $i); $j++) {
         echo "&nbsp";
     }
     // 第3排決定每排打幾個星星
@@ -76,9 +79,9 @@ for ($i = 0; $i < 5; $i++) {
 <h1>星星倒三角形</h1>
 <?php
 // 第1排決定共幾排星星
-for ($i = 4; $i >= 0; $i--) {
+for ($i = $amount; $i >= 0; $i--) {
     //  第2排決定每排打幾個空排
-    for ($j = 0; $j < (4 - $i); $j++) {
+    for ($j = 0; $j < ($amount - $i); $j++) {
         echo "&nbsp";
     }
     // 第3排決定每排打幾個星星
@@ -90,16 +93,16 @@ for ($i = 4; $i >= 0; $i--) {
 ?>
 <h1>星星菱形</h1>
 <?php
-
-for ($i = 0; $i < 9; $i++) {
-
-    if ($i <= 4) {
+$mid=floor(($amount*2-1)/2);
+for ($i = 0; $i <($amount*2-1); $i++) {
+// floor()無條件捨去、ceil無條件進位、ronud四捨五入
+    if ($i <= $mid) {
         $tmp = $i;
     } else {
         $tmp--;
     }
 
-    for ($j = 0; $j < (4 - $tmp); $j++) {
+    for ($j = 0; $j < ($mid - $tmp); $j++) {
         echo "&nbsp";
     }
 
@@ -112,12 +115,12 @@ for ($i = 0; $i < 9; $i++) {
 ?>
 <h1>星星矩形</h1>
 <?php
-for ($i = 0; $i < 7; $i++) {
+for ($i = 0; $i < $amount; $i++) {
 
-    for ($j = 0; $j < 7; $j++) {
-        if ($i == 0 || $i == 6) {
+    for ($j = 0; $j < $amount; $j++) {
+        if ($i == 0 || $i == ($amount-1)) {
             echo "*";
-        } else if ($j == 0 || $j == 6) {
+        } else if ($j == 0 || $j == ($amount-1)) {
             echo "*";
         } else {
             echo "&nbsp";
@@ -137,14 +140,14 @@ for ($i = 0; $i < 7; $i++) {
     -->
 <h1>星星矩形大魔王</h1>
 <?php
-for ($i = 0; $i < 7; $i++) {
+for ($i = 0; $i < $amount; $i++) {
 
-    for ($j = 0; $j < 7; $j++) {
-        if ($i == 0 || $i == 6) {
+    for ($j = 0; $j < $amount; $j++) {
+        if ($i == 0 || $i == ($amount-1)) {
             echo "*";
-        } else if ($j == 0 || $j == 6 ) {
+        } else if ($j == 0 || $j == ($amount-1 )) {
             echo "*";
-        }else if($i==$j || $i+$j==6){
+        }else if($i==$j || $i+$j==($amount-1)){
             // span行內標籤
             echo "<span style='color:red'>*</span>";
         }
