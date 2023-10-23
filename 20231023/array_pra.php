e279cdd6cf33ae44f6bc902a07027b1bcd3ecccc
cable <cable870526@gmail.com>
2023-10-23 10:36:06 +0800

    陣列設計

---
+++ 20231023/array_pra.php
@@ -0,0 +1,119 @@
+<?php
+
+$test = [
+    "國文" => [
+        [
+            "name" => "judy",
+            "score" => "95"
+        ],
+        [
+            "name" => "amo",
+            "score" => "88"
+        ],
+        [
+            "name" => "john",
+            "score" => "45"
+        ],
+        [
+            "name" => "peter",
+            "score" => "59"
+        ],
+        [
+            "name" => "hebe",
+            "score" => "71"
+        ],
+    ],
+    "英文" => [
+        [
+            "name" => "judy",
+            "score" => "64"
+        ],
+        [
+            "name" => "amo",
+            "score" => "78"
+        ],
+        [
+            "name" => "john",
+            "score" => "60"
+        ],
+        [
+            "name" => "peter",
+            "score" => "32"
+        ],
+        [
+            "name" => "hebe",
+            "score" => "62"
+        ],
+    ],
+    "數學" => [
+        [
+            "name" => "judy",
+            "score" => "70"
+        ],
+        [
+            "name" => "amo",
+            "score" => "54"
+        ],
+        [
+            "name" => "john",
+            "score" => "68"
+        ],
+        [
+            "name" => "peter",
+            "score" => "77"
+        ],
+        [
+            "name" => "hebe",
+            "score" => "80"
+        ],
+    ],
+    "地理" => [
+        [
+            "name" => "judy",
+            "score" => "90"
+        ],
+        [
+            "name" => "amo",
+            "score" => "81"
+        ],
+        [
+            "name" => "john",
+            "score" => "70"
+        ],
+        [
+            "name" => "peter",
+            "score" => "54"
+        ],
+        [
+            "name" => "hebe",
+            "score" => "62"
+        ],
+    ],
+    "歷史" => [
+        [
+            "name" => "judy",
+            "score" => "84"
+        ],
+        [
+            "name" => "amo",
+            "score" => "71"
+        ],
+        [
+            "name" => "john",
+            "score" => "62"
+        ],
+        [
+            "name" => "peter",
+            "score" => "42"
+        ],
+        [
+            "name" => "hebe",
+            "score" => "64"
+        ],
+    ]
+
+];
+
+echo"<pre>";
+print_r($test);
+echo"</pre>";
