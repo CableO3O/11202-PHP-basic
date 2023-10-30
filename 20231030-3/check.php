402d35032a0ee64460c12db2a92306cdd25c0673
cable <cable870526@gmail.com>
2023-10-30 13:58:07 +0800

    include、require用法

--- 20231030-3/check.php
+++ 20231030-3/check.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("Asia/Taipei");
 if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
 
-    setcookie("login","mackliu",time()+60);
+    setcookie("login","cable",time()+60);
 
     header("location:member.php");
 
