5dbf463b385ef281501639e06e899e1ea001fef8
cable <cable870526@gmail.com>
2023-10-30 14:37:38 +0800

    include中增加邏輯判斷、共用函式

--- 20231030-4/member.php
+++ 20231030-4/member.php
@@ -1,3 +1,6 @@
+<?php 
+include_once "session.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +14,9 @@
 include_once "header.php";
 ?>
 <?php
-session_start();
 if(isset($_SESSION['login']) && !empty($_SESSION['login'])){
     echo "<h3>登入成功</h3>";
-    echo "<a href='login.php'>回登入頁</a>";
-    echo "<a href='logout.php'>登出</a>";
+    
 }else{
     $_SESSION['error']="沒有登入相關驗證，非法登入";
    header("location:login.php");
