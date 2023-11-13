117a33d53f89ed232ea74095b8ee1c02290de048
cable <cable870526@gmail.com>
2023-11-13 16:01:16 +0800

    優化判斷

--- 20231113-2/member/login.php
+++ 20231113-2/member/login.php
@@ -7,9 +7,10 @@ $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
 
 $sql="select * from user where `acc`='$acc' && `pw`='$pw'";
-$user=$pdo->query($sql)->fetch();
-
-if($user['acc']==$acc && $user['pw']==$pw){
+// fetchColumn()直接確認取出的值是否符合
+$user=$pdo->query($sql)->fetchColumn();
+// if($user['acc']==$acc && $user['pw']==$pw){
+if($user){
     $_SESSION['user']=$acc;
     header("location:index.php");
 }else{
