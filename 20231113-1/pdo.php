4cd0386d4380f0d52bbc1ecd24355e1c5e4ac80e
cable <cable870526@gmail.com>
2023-11-13 11:02:34 +0800

    php與sql結合運用

---
+++ 20231113-1/pdo.php
@@ -0,0 +1,27 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');  
+
+// $sql="select * from students";
+// $rows=$pdo->query($sql)->fetchAll();
+
+
+// echo "<pre>";
+// print_r($rows[0][2]);
+// print_r($rows[0]['parents']);
+// echo "</pre>";
+
+// $sql="insert into `classes`(`code`,`name`,`tutor`) values('111','一年十一班','安安安');";
+// $pdo->query($sql);
+
+// $sql="update `classes` set `code`='110',`name`='一年十班',`tutor`='哈哈哈' where `id`='10'";
+// $pdo->query($sql);
+
+
+// $sql="delete from `classes` where `id`='11'";
+// $pdo->query($sql);
+
+
+
+$sql="delete from `classes` where `id`='10'";
+echo $pdo->exec($sql);
