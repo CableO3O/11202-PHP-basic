52a9404c9b65f1ce969722c920d2a80460b40f23
cable <cable870526@gmail.com>
2023-11-16 15:45:35 +0800

    註冊帳號檢查有無重複帳號、電子郵件

--- 20231113-2/member/add_user.php
+++ 20231113-2/member/add_user.php
@@ -1,22 +1,21 @@
 <?php
-session_start();
-$dsn="mysql:host=localhost;charset=utf8;dbname=member";
-$pdo=new PDO($dsn,'root','');
-$acc=htmlspecialchars(trim($_POST['acc']));
-$pw=htmlspecialchars(trim($_POST['pw']));
-$name=htmlspecialchars(trim($_POST['name']));
-$email=htmlspecialchars(trim($_POST['email']));
-$address=htmlspecialchars(trim($_POST['address']));
-$checksql="select * from `user` where `acc`='$acc' || `email`='$email'";
+$dsn = "mysql:host=localhost;charset=utf8;dbname=member";
+$pdo = new PDO($dsn, 'root', '');
+$acc = htmlspecialchars(trim($_POST['acc']));
+$pw = htmlspecialchars(trim($_POST['pw']));
+$name = htmlspecialchars(trim($_POST['name']));
+$email = htmlspecialchars(trim($_POST['email']));
+$address = htmlspecialchars(trim($_POST['address']));
 
-$checkuser=$pdo->query($checksql)->fetchColumn();
+$checksql = "select * from `user` where `acc`='$acc' || `email`='$email'";
+$checkuser = $pdo->query($checksql)->fetchColumn();
 
 if ($checkuser) {
     header("location:reg.php?error=已有重複帳戶或電子郵件註冊帳號，請輸入其他資料");
-}else{
-$sql="insert into `user`(`acc`,`pw`,`name`,`email`,`address`)
-                   values('{$acc}','{$pw}','{$name}','{$email}','{$address}')" ;
-$pdo->exec($sql);
+} else {
+    $sql = "insert into `user`(`acc`,`pw`,`name`,`email`,`address`)
+                   values('{$acc}','{$pw}','{$name}','{$email}','{$address}')";
+    $pdo->exec($sql);
 
-header("location:index.php");
+    header("location:index.php");
 }
