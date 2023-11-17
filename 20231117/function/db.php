2a199c50dd57b23e76332ad094b400c569f64ce6
cable <cable870526@gmail.com>
2023-11-17 16:20:37 +0800

    all() 函式增加第三參數的功能

--- 20231117/function/db.php
+++ 20231117/function/db.php
@@ -1,5 +1,5 @@
 <?php
-function all($data_sheet = null, $where = '')
+function all($data_sheet = null, $where = '',$other='')
 {   
     $dsn = 'mysql:host=localhost;charset=utf8;dbname=school';
     $pdo = new PDO($dsn, 'root', '');
@@ -20,7 +20,11 @@ function all($data_sheet = null, $where = '')
         } else {
             $sql .="  $where";
         }
-        $rows = $pdo->query($sql)->fetchAll();
+            $sql .=" $other";
+        // fetchAll(PDO::FETCH_ASSOC)讓出來的資料只有標籤
+        // fetchAll(PDO::FETCH_NUM)讓出來的資料只有索引
+
+        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         return $rows;
     } else {
         echo "錯誤:沒有指定的資料表名稱";
@@ -33,6 +37,6 @@ function dd($array)
     echo "</pre>";
 }
 
-$rows = all('students', "");
+$rows = all('students',['dept'=>'1'],"order by id desc");
 
 dd($rows);
