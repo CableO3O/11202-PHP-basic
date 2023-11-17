c1b4b859a376237b9acce81901777554e3f08def
cable <cable870526@gmail.com>
2023-11-17 16:04:15 +0800

    all() 函式增加第二參數的功能

--- 20231117/function/db.php
+++ 20231117/function/db.php
@@ -1,22 +1,38 @@
 <?php
-$rows=all('students',"where dept='1'");
+function all($data_sheet = null, $where = '')
+{   
+    $dsn = 'mysql:host=localhost;charset=utf8;dbname=school';
+    $pdo = new PDO($dsn, 'root', '');
+    $sql="select * from `$data_sheet`";
 
-dd($rows);
-
-function all($data_sheet=null,$where=''){
-    $dsn='mysql:host=localhost;charset=utf8;dbname=school';
-    $pdo=new PDO($dsn,'root','');
-    
     if (isset($data_sheet) && !empty($data_sheet)) {
-        $sql="select * from `$data_sheet` $where";
-        $rows=$pdo->query($sql)->fetchAll();
+
+        if (is_array($where)) {
+            
+            if (!empty($where)) {
+                foreach ($where as $col => $value) {
+                    $tmp[]="`$col`='$value'";
+                }
+                $sql .=" where ".join(" && ",$tmp);
+            }
+
+
+        } else {
+            $sql .="  $where";
+        }
+        $rows = $pdo->query($sql)->fetchAll();
         return $rows;
-    }else{
+    } else {
         echo "錯誤:沒有指定的資料表名稱";
     }
 }
-function dd($array){
-    echo"<pre>";
+function dd($array)
+{
+    echo "<pre>";
     print_r($array);
-    echo"</pre>";
+    echo "</pre>";
 }
+
+$rows = all('students', "");
+
+dd($rows);
