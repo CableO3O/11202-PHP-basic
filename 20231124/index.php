b646242534471b40e3c4bc30c7519911d6a8ac77
cable <cable870526@gmail.com>
2023-11-24 11:02:52 +0800

    繼承建構式

--- 20231124/index.php
+++ 20231124/index.php
@@ -21,14 +21,31 @@ class Animal{
 
 //實例化 instant
 // 使用__constructr建立函式參數就要先在實例化的地方將參數打上
-$animal=new Animal('阿明');
+// $animal=new Animal('阿明');
 
 
-echo '顯示名稱:'.$animal->getName();
-echo '<br>';
-$animal->setName('小花');
-echo '顯示名稱:'.$animal->getname();
+// echo '顯示名稱:'.$animal->getName();
+// echo '<br>';
+// $animal->setName('小花');
+// echo '顯示名稱:'.$animal->getname();
 // $animal->name='阿忠';
 // echo '<br>';
 // echo '顯示名稱:'.$animal->name;
 
+
+// 類別Dog繼承類別Animal(Dog可以使用Animal的功能並擴充)
+class Dog extends Animal{
+     
+    function sit(){
+        echo $this->name;
+        echo "坐下";
+    }
+}
+
+$dog=new Dog("一毛二");
+
+echo $dog->getname();
+echo "<br>";
+echo $dog->setname("二毛四");
+echo $dog->getname();
+$dog->sit();
