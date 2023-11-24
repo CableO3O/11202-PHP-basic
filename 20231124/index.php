14684ed7381fe4c7f7446e61f5cb4b993f9520d7
cable <cable870526@gmail.com>
2023-11-24 10:39:05 +0800

    建構式

--- 20231124/index.php
+++ 20231124/index.php
@@ -2,21 +2,33 @@
 
 
 class Animal{
-    public $name;
+    protected $name;
+
+    // 在被實例化後就先執行
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
 
     public function setName($name){
         $this->name=$name;
+    }
+    public function getName(){
+        return $this->name;
     }
 
 }
 
 //實例化 instant
-$animal=new Animal;
+// 使用__constructr建立函式參數就要先在實例化的地方將參數打上
+$animal=new Animal('阿明');
 
 
-echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getName();
 echo '<br>';
 $animal->setName('小花');
-echo '<br>';
-echo '顯示名稱:'.$animal->name;
+echo '顯示名稱:'.$animal->getname();
+// $animal->name='阿忠';
+// echo '<br>';
+// echo '顯示名稱:'.$animal->name;
 
