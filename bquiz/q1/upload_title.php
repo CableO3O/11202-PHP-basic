212de4f861ea7deea12a9b4de60e0e4bfbb78879
cable <cable870526@gmail.com>
2023-12-01 08:54:53 +0800

    建立更新圖片

--- bquiz/q1/upload_title.php
+++ bquiz/q1/upload_title.php
@@ -1,13 +1,14 @@
-<h3 class="text-center">更新標題圖片</h3>
+<h3 class="text-center">更新標題圖片<?= $_GET['id']; ?></h3>
 <form action="add_title.php" method="post" enctype="multipart/form-data">
     <table class="col-8 m-auto">
         <tr>
             <td>標題區圖片</td>
             <td><input type="file" name="img" id=""></td>
+            <input type="hidden" name="id" value="<?= $_GET['id'];?>">
         </tr>
     </table>
     <div class="text-center">
-        <input type="submit" value="新增">
+        <input type="submit" value="更新">
         <input type="reset" value="重置">
     </div>
 </form>
