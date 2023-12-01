a35efaf1a1f6d4a66b39605cdfdc64126d5a24c4
cable <cable870526@gmail.com>
2023-12-01 15:03:29 +0800

    完成投票紀錄功能

--- bquiz/q2/vote.php
+++ bquiz/q2/vote.php
@@ -16,20 +16,20 @@
         ?>
         <fieldset>
             <legend>目前位置 : 首頁 > 問卷調查 ><?= $ques['text']; ?></legend>
-            <form action="" method="post">
-                <h2 class="text-center"><?= $ques['text']; ?></h2>
+            <h2 class="text-center"><?= $ques['text']; ?></h2>
+            <form action="./api/vote.php" method="post">
+                <ul class="list-group col-6 mx-auto">
                 <?php
                 $allques = $Que->all(['subject_id' => $_GET['id']]);
                 foreach ($allques as $allidx => $allque) {
                 ?>
-                    <ul class="list-group col-6 mx-auto">
                         <li class="list-group-item list-group-item-action">
                             <input type="radio" name="allque" id="" value="<?= $allque['id']; ?>"><?= $allque['text']; ?>
                         </li>
-                    </ul>
                 <?php
                 }
                 ?>
+                    </ul>
                 <input type="submit" value="我要投票" class="btn btn-primary d-block mx-auto mt-3">
             </form>
         </fieldset>
