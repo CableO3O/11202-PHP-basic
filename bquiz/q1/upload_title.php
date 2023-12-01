9d6740fd0aef6205698003604a0c88193e00b84e
cable <cable870526@gmail.com>
2023-12-01 08:33:44 +0800

    建立更新圖片

---
+++ bquiz/q1/upload_title.php
@@ -0,0 +1,13 @@
+<h3 class="text-center">更新標題圖片</h3>
+<form action="add_title.php" method="post" enctype="multipart/form-data">
+    <table class="col-8 m-auto">
+        <tr>
+            <td>標題區圖片</td>
+            <td><input type="file" name="img" id=""></td>
+        </tr>
+    </table>
+    <div class="text-center">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
