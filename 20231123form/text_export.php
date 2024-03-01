eee6f46e005ed37ab44c6368cff8296522fb98a6
cable <cable870526@gmail.com>
2024-03-01 11:49:48 +0800

    加入全選功能

--- 20231123form/text_export.php
+++ 20231123form/text_export.php
@@ -1,18 +1,18 @@
 <?php
 include_once "db_export.php";
 if (!empty($_POST)) {
-    $rows = all("20240301","where `年別` in ('".join("','",$_POST['select'])."')");
-    $filename=date("Ymd").rand(100000000,999999999);
-    $file=fopen("./doc/{$filename}.csv",'w+');
-    fwrite($file,"\xEF\xBB\xBF");
-    $chk=false;
+    $rows = all("20240301", "where `年別` in ('" . join("','", $_POST['select']) . "')");
+    $filename = date("Ymd") . rand(100000000, 999999999);
+    $file = fopen("./doc/{$filename}.csv", 'w+');
+    fwrite($file, "\xEF\xBB\xBF");
+    $chk = false;
     foreach ($rows as $row) {
         if (!$chk) {
-            $cols=array_keys($row);
-            fwrite($file,join(",",$cols)."\r\n");
-            $chk=true;
+            $cols = array_keys($row);
+            fwrite($file, join(",", $cols) . "\r\n");
+            $chk = true;
         }
-        fwrite($file,join(",",$row)."\r\n");
+        fwrite($file, join(",", $row) . "\r\n");
     }
     fclose($file);
     echo "<a href='./doc/{$filename}.csv' download>檔案已匯出,請點此連結下載</a>";
@@ -40,7 +40,7 @@ if (!empty($_POST)) {
     <input type="submit" value="匯出選擇的資料">
     <table>
         <tr>
-            <th>勾選</th>
+            <th><input type="checkbox" name="" id="select">勾選</th>
             <th>年別</th>
             <th>公園綠地－男</th>
             <th>公園綠地－女</th>
@@ -54,7 +54,7 @@ if (!empty($_POST)) {
         foreach ($rows as $row) {
             echo "<tr>";
             echo "<td>";
-            echo "<input class='select' type='checkbox' name='select[]' value='{$row['年別']}'>";
+            echo "<input type='checkbox' name='select[]' value='{$row['年別']}'>";
             echo "</td>";
             foreach ($row as $value) {
                 echo "<td>";
@@ -66,3 +66,13 @@ if (!empty($_POST)) {
         ?>
     </table>
 </form>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+$("#select").on("change",function(){
+    if ($(this).prop('checked')) {
+        $("input[name='select[]']").prop('checked',true);
+    }else{
+        $("input[name='select[]']").prop('checked',false);
+    }
+})
+</script>
